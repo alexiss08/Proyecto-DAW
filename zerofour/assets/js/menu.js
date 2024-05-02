96540f410c2a94c0564d569aa7921cf191f51731
Victor120y <ha22027@ues.edu.sv>
2024-05-01 21:18:02 -0600

    agregandome menu de javascript

---
+++ zerofour/assets/js/menu.js
@@ -0,0 +1,37 @@
+document.write("<header id='header'>");
+document.write("    <div class='inner'>");
+
+document.write("        <!-- Logo -->");
+document.write("        <h1><a href='index.html' id='logo'>IDS-Solutions</a></h1>");
+
+document.write("        <!-- Nav -->");
+document.write("        <nav id='nav'>");
+document.write("            <ul>");
+document.write("                <li class='current_page_item'><a href='index.html'>Inicio</a></li>");
+document.write("                <!-- Ocultando menu desplegable");
+document.write("                <li>");
+document.write("                    <a href='#'>Dropdown</a>");
+document.write("                    <ul>");
+document.write("                        <li><a href='#'>Lorem ipsum dolor</a></li>");
+document.write("                        <li><a href='#'>Magna phasellus</a></li>");
+document.write("                        <li>");
+document.write("                            <a href='#'>Phasellus consequat</a>");
+document.write("                            <ul>");
+document.write("                                <li><a href='#'>Lorem ipsum dolor</a></li>");
+document.write("                                <li><a href='#'>Phasellus consequat</a></li>");
+document.write("                                <li><a href='#'>Magna phasellus</a></li>");
+document.write("                                <li><a href='#'>Etiam dolore nisl</a></li>");
+document.write("                            </ul>");
+document.write("                        </li>");
+document.write("                        <li><a href='#'>Veroeros feugiat</a></li>");
+document.write("                    </ul>");
+document.write("                </li>");
+document.write("												-->");
+document.write("                <li><a href='left-sidebar.html'>Quienes somos</a></li>");
+document.write("                <li><a href='right-sidebar.html'>Nuestros Clientes</a></li>");
+document.write("                <li><a href='no-sidebar.html'>Equipos</a></li>");
+document.write("                <li><a href='no-sidebar.html'>Contactanos</a></li>");
+document.write("            </ul>");
+document.write("        </nav>");
+document.write("    </div>");
+document.write("</header>");
